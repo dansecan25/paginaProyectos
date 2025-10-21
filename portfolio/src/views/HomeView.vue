e075f58faaf182835011080648c1af7f57a8b542
Daniel Serrano Cañas <51306347+dansecan25@users.noreply.github.com>
2025-10-20 20:02:38 -0600

    [UPDATE] text styles

--- portfolio/src/views/HomeView.vue
+++ portfolio/src/views/HomeView.vue
@@ -1,20 +1,20 @@
 <template>
   <section class="testing-class">
     <p class="title-container">Project portfolio</p>
-    <h1>
+    <h1 class="main-header">
       My name is Daniel,<br />
       Happy to learn and build always!
     </h1>
-    <p>
+    <p class="bio-description">
       I'm a computer engineer who loves programming and learning new technologies.
-      My main interests are in the fields of computer architecture and quantum computer architectures!
+      My main interests are in the fields of computer architecture and quantum computer architectures!<br />
       Skilled in adapting to new technologies fast and efficiently.
     </p>
-    <button>Contact</button>
+    <button class="contact-button">Contact</button>
   </section>
 
   <section>
-    <h2 >
+    <h2 class="abilities">
       I’m capable of building websites, mobile apps for Android and IOS, mainframe batch jobs, and more!
     </h2>
     <p >
@@ -42,12 +42,35 @@
   /* Estilos específicos para HomeView.vue */
   .title-container {
     display:flex;
-   padding:2px 20px;
+   padding:4px 20px;
    margin: 84px 10px 44px 0px;
    width: fit-content;
    border-radius: 4px;
    background-color: #423F43;
   }  
-  
+  .main-header {
+    margin:0px 0px 13px 0px;
+  }
+  .bio-description {
+    width: 70%;
+    margin:0px 0px 13px 0px;
+    font-size:15;
+    justify-content:baseline;
+  }
+  .contact-button {
+    background-color: #ffffff;
+    color: #000000;
+    margin:73px 0px 245px 0px;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  .abilities {
+    margin:0px 0px 74px 0px;
+    font-size: 48px;
+    width: 85%;
+    font-weight: normal;
+  }
   
 </style>
